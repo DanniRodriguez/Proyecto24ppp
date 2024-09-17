f395c6aceb18321725ab4910dd61aa9c0393c8bc
Anonimo <rodriguezromerovaleria@pedropoveda.edu.bo>
2024-09-17 11:38:24 -0400

    Create 3catalogo.php

---
+++ catalogo.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="pagina3.css" rel="stylesheet" type="text/css" />
+    <title>Catalogo</title>
+</head>
+<body>
+<div id="catalogo"></div>
+<h1>SERVICIOS</h1>
+<div id="servicios">
+    <div class="texto">
+        <h2 id="cap" class="tituloS">CAPACITACIONES</h2>
+            <p class="descripS">Ofrecemos cursos educativos sobre la gestión sostenible de residuos 
+                y la implementación de prácticas ecológicas en comunidades y organizaciones. 
+                Nuestras capacitaciones están diseñadas para empoderar a los participantes con 
+                conocimientos y habilidades en compostaje, vermicompostaje, huertos urbanos, y la 
+                creación de soluciones medioambientales efectivas.
+            </p>
+    </div>
+    <img src="capacitacion.png" id="capI" class="imagen">
+    <img src="taller2.jpg" id="talI" class="imagen">
+    <div class="texto">
+        <h2 id="tal"  class="tituloS">TALLERES</h2>
+            <p class="descripS">Enseñamos a convertir los residuos orgánicos en compost y humus de alta calidad 
+                mediante métodos naturales. Nuestros talleres incluyen teoría y práctica, 
+                capacitando a los participantes para implementar técnicas de compostaje y 
+                vermicompostaje en sus hogares, escuelas o comunidades, reduciendo así la cantidad 
+                de residuos y mejorando la fertilidad del suelo.
+            </p>
+    </div>
+    <div class="texto">
+        <h2 id="con"  class="tituloS">CONSULTORÍAS</h2>
+        <p class="descripS">Asesoramos a empresas, comunidades y gobiernos en la planificación e implementación 
+            de proyectos verdes que promuevan la sostenibilidad y la economía circular. 
+            Ayudamos a diseñar e implementar programas de compostaje, gestión de residuos, 
+            huertos urbanos, y otras iniciativas ecológicas adaptadas a las necesidades locales.
+        </p>
+    </div>
+    <img src="consulta2.JPG" class="imagen" id="conI">
+</div>
+<center><h2 id="e">¿Le interesa alguno de nuestros servicios?</h2></center>
+<center><button>¡coticemos!</button></center>
+<h1>PRODUCTOS</h1>
+<div id="productos">
+    <div class="contprod">
+        <h3>BIOL</h3>
+        <img src="Biol1.png">
+        <p class="DescripP"></p>
+    </div>
+    <div class="contprod">
+        <h3>COMPOST</h3>
+        <img  src="CompostHuk.png">
+    </div>
+    <div class="contprod">
+        <h3>HUMUS</h3>
+        <img src="humusHuk.png">
+    </div>
+    <div class="contprod" id="Ul">
+        <h3>LOMBRICES</h3>
+        <img src="lombrizHuk.png">
+    </div>
+</div>
+</body>
+</html>
