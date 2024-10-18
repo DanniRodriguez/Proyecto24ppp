28179446daf8172695d23b29673c7d9d076de074
nlaramorillas <laramorillasnatalia@pedropoveda.edu.bo>
2024-10-18 11:23:15 -0400

    Update 2.6 Catalogo.php

--- 2.6 Catalogo.php
+++ 2.6 Catalogo.php
@@ -4,7 +4,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="2.5 Catalogo.css" rel="stylesheet" type="text/css" />
     <title>Catalogo</title>
 </head>
 <style>
