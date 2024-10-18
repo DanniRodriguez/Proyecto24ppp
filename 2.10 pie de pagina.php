3d208ff927f0da096184140a3077c6e73dc12be0
nlaramorillas <laramorillasnatalia@pedropoveda.edu.bo>
2024-10-18 11:22:56 -0400

    Update 2.10 pie de pagina.php

--- 2.10 pie de pagina.php
+++ 2.10 pie de pagina.php
@@ -5,7 +5,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <link rel="stylesheet" href="2.9 pie de pagina.css">
     <title>Pie de página</title>
 </head>
 <style>
